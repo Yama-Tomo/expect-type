c633f7e26c96c7830754fefb727d4022c47c766b
Yama-Tomo <yam4.tomo@gmail.com>
2022-09-02 14:05:15 +0900

    test: add tests

--- src/core.test.ts
+++ src/core.test.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { expect, test, vi } from 'vitest';
-import { type, configure } from './core';
+import { type, configure, Options as AliasedOptions, Options } from './core';
 
 type Test1 = Array<string | number>;
 interface Test2 {
@@ -10,6 +10,11 @@ interface Test2 {
 test('extract type string', () => {
   expect(type('Test1')).toBe('(string | number)[]');
   expect(type('Test2')).toBe('Test2');
+
+  // prettier-ignore
+  const coreOptionsType = '{ compilerOptions?: CompilerOptions | undefined; testFilePath?: ((filePath: string) => boolean) | undefined; typeFormat?: TypeFormatFlags | undefined; }';
+  expect(type('AliasedOptions')).toBe(coreOptionsType);
+  expect(type('Options')).toBe(coreOptionsType);
 });
 
 test('throw error when unable to find types', () => {
